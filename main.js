4f041fbc2e0c1287cffe3fba2d58190ade8f1c3d
PollyanaAGW <pollyanadias91@gmail.com>
2024-08-22 14:57:28 -0300

    cubo girando

--- main.js
+++ main.js
@@ -6,27 +6,27 @@ let vel_t = 0.1;*/
 
 class Cube{
 	constructor(cube){
-		let min = -0.1;
+		let min = 0.1;
 		let max = 0.1;
 		this.vel_h = min + (max - min) * Math.random();
 		this.vel_v = min + (max - min) * Math.random();
-		this.vel_t = min + (max - min) * Math.random();
+		//this.vel_t = min + (max - min) * Math.random();
 		this.cube = cube;
 	}
 
 	update(){
 		this.cube.position.x += this.vel_h;
-		if (this.cube.position.x > 5 || this.cube.position.x < -5 ) {
+		/*if (this.cube.position.x > 5 || this.cube.position.x < -5 ) {
 			this.vel_h = -this.vel_h;
-		}
+		}*/
 		this.cube.position.y += this.vel_v;
-		if (this.cube.position.y > 2 || this.cube.position.y < -2) {
+		/*if (this.cube.position.y > 2 || this.cube.position.y < -2) {
 			this.vel_v = -this.vel_v;
-		}
-		this.cube.position.z += this.vel_t;
+		}*/
+		/*this.cube.position.z += this.vel_t;
 		if (this.cube.position.z > 2 || this.cube.position.z < -2) {
 			this.vel_t = -this.vel_t
-		}
+		}*/
 	}
 }
 
@@ -42,7 +42,7 @@ const material = new THREE.MeshBasicMaterial( { color: 0x00ff00, wireframe: true
 
 let cubes = [];
 
-for (let i = 0; i < 2; i++){
+for (let i = 0; i < 1; i++){
 	const cube = new Cube (new THREE.Mesh( geometry, material ));
 	scene.add( cube.cube );
 	cubes.push(cube);
@@ -52,12 +52,19 @@ for (let i = 0; i < 2; i++){
 
 
 camera.position.z = 5;
+let raio = 3;
+let tetha = 0.0;
 
 function animate() {
 	renderer.render( scene, camera );
 
+	tetha += 0.1;
+	
 	for (var cube of cubes)
 		cube.update();
+		cube.cube.position.x = raio * Math.cos(tetha);
+		cube.cube.position.y = raio * Math.sin(tetha);
+
 	/*
 	cube.position.x += vel_h;
 	cube.position.y += vel_v;
